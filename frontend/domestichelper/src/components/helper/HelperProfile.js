44b1e45fd0bc43a1d541409eebd39b0706353eb7
mukund khilare <mukundkhilare123@gmail.com>
2023-09-03 19:25:12 +0530

    Designing of Userprofile.js and Helperprofile.js

--- frontend/domestichelper/src/components/helper/HelperProfile.js
+++ frontend/domestichelper/src/components/helper/HelperProfile.js
@@ -1,15 +1,79 @@
 import React from 'react';
-
-
-
+import './HelperProfile.css'; // Import your custom CSS file
 
 const HelperProfile = () => {
-    const storedUserData = localStorage.getItem('LoginHelperData');
-const userData = storedUserData;
+  const storedUserData = localStorage.getItem('LoginHelperData');
+  const userData = JSON.parse(storedUserData);
 
   return (
-    <div>
-      <h2>Welcometo the Home Page{userData}</h2>
+    <div className="container HelperProfile">
+      <h2>Welcome to the Home Page</h2>
+      <div className="helper-info">
+        <div className="row">
+          <div className="col-md-4">
+            <strong>Helper Name:</strong>
+          </div>
+          <div className="col-md-8">
+            <span>{userData.helperName}</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <strong>Email:</strong>
+          </div>
+          <div className="col-md-8">
+            <span>{userData.helperEmail}</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <strong>Mobile Number:</strong>
+          </div>
+          <div className="col-md-8">
+            <span>{userData.helperMobileNumber}</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <strong>Address:</strong>
+          </div>
+          <div className="col-md-8">
+            <span>{userData.helperAddress}</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <strong>City:</strong>
+          </div>
+          <div className="col-md-8">
+            <span>{userData.helperCity}</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <strong>Pincode:</strong>
+          </div>
+          <div className="col-md-8">
+            <span>{userData.helperPincode}</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <strong>Service Name:</strong>
+          </div>
+          <div className="col-md-8">
+            <span>{userData.servicename}</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <strong>Is Available:</strong>
+          </div>
+          <div className="col-md-8">
+            <span>{userData.isAvalible ? 'Yes' : 'No'}</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/domestichelper/src/components/user/UserProfile.js
+++ frontend/domestichelper/src/components/user/UserProfile.js
@@ -1,16 +1,71 @@
 import React from 'react';
-
-
-
+import './UserProfile.css'; // Import your custom CSS file
 
 const UserProfile = () => {
-    const storedUserData = localStorage.getItem('UserLoginData');
-const userData = storedUserData;
-
+  const storedUserData = localStorage.getItem('UserLoginData');
+  const userData = JSON.parse(storedUserData);
 
   return (
-    <div>
-      <h2>Welcome to the Home Page{userData}</h2>
+    <div className="container UserProfile">
+      <h2>Welcome to the Home Page</h2>
+      <div className="user-info">
+        <div className="row">
+          <div className="col-md-4">
+            <strong>User Name:</strong>
+          </div>
+          <div className="col-md-8">
+            <span>{userData.userName}</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <strong>Email:</strong>
+          </div>
+          <div className="col-md-8">
+            <span>{userData.userEmail}</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <strong>Mobile Number:</strong>
+          </div>
+          <div className="col-md-8">
+            <span>{userData.userMobileNumber}</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <strong>Address:</strong>
+          </div>
+          <div className="col-md-8">
+            <span>{userData.userAddress}</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <strong>Aadhar Card:</strong>
+          </div>
+          <div className="col-md-8">
+            <span>{userData.userAdharcard}</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <strong>City:</strong>
+          </div>
+          <div className="col-md-8">
+            <span>{userData.userCity}</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <strong>Pincode:</strong>
+          </div>
+          <div className="col-md-8">
+            <span>{userData.userPincode}</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
